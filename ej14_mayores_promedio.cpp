2a2efc6d40666b883d28aebdce65218f6dcb0956
alangaquinor <alangaquinor20@gmail.com>
2025-09-16 16:21:31 -0300

    Ejercicio 14: Elementos mayores que el promedio.

---
+++ ej14_mayores_promedio.cpp
@@ -0,0 +1,31 @@
+// Realice un programa que defina un vector de números y determine cuántos elementos son mayores que el promedio de los elementos del vector.
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    
+    vector<int> numeros = {10, 20, 30, 40, 50};
+
+    double suma = 0;
+    for (int num : numeros) {
+        suma += num;
+    }
+
+    double promedio = suma / numeros.size();
+
+    int contador = 0;
+    for (int num : numeros) {
+        if (num > promedio) {
+            contador++;
+        }
+    }
+
+    cout << "Promedio: " << promedio << endl;
+    cout << "Cantidad de elementos mayores que el promedio: " << contador << endl;
+
+    return 0;
+}
+
